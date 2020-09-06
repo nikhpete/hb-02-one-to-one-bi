dbb5af913caf59e0213dc3736efb8adab583012c
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-09-06 20:11:19 +0530

    one-to-one bidirectional instructor from instructorDetail

---
+++ src/com/nick/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,41 @@
+package com.nick.hibernate.demo;
+
+import java.text.ParseException;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.nick.hibernate.demo.entity.Instructor;
+import com.nick.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) throws ParseException {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			session.beginTransaction();
+
+			InstructorDetail insDet = session.get(InstructorDetail.class, 2);
+			
+			System.out.println(insDet);
+			
+			System.out.println(insDet.getInstructor());
+			
+			session.getTransaction().commit();
+
+		} finally {
+			factory.close();
+		}
+	}
+
+}
---
+++ src/com/nick/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,47 @@
+package com.nick.hibernate.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "instructor")
+@NoArgsConstructor
+@Data
+public class Instructor {
+
+	public Instructor(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetail instructorDetial;
+
+}
